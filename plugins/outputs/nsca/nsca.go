26b07c6b70ef3fa1ec4e5f74c0993e22dd2e8e2b
Irappa Pattar <irappapattar30@gmail.com>
2018-01-11 18:20:50 +0530

    configured Key & Indentity

--- plugins/outputs/nsca/nsca.go
+++ plugins/outputs/nsca/nsca.go
@@ -1,14 +1,11 @@
 package nsca
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
-	"io"
 	"net"
 	"strconv"
-	"time"
 
+	"github.com/google/uuid"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/outputs"
 	"github.com/influxdata/telegraf/plugins/serializers"
@@ -16,39 +13,14 @@ import (
 	"github.com/raff/tls-psk"
 )
 
-// ServerInfo contains the configuration information for an NSCA server
-type ServerInfo struct {
-	// Host is the IP address or host name of the NSCA server. Leave empty for localhost.
-	Host string
-	// Port is the IP port number (no default)
-	Port string
-	// EncryptionMethod specifies the message encryption to use on NSCA messages. It defaults to ENCRYPT_NONE.
-	EncryptionMethod int
-	// Password is used in encryption.
-	Password string
-	// Timeout is the connect/read/write network timeout
-	Timeout time.Duration
-}
-
-// Message is the contents of an NSCA message
-type message struct {
-	// State is one of {STATE_OK, STATE_WARNING, STATE_CRITICAL, STATE_UNKNOWN}
-	state string
-	// Host is the host name to set for the NSCA-ng message
-	host string
-	// Service is the service name to set for the NSCA-ng message
-	service string
-	// Message is the "plugin output" of the NSCA-ng message
-	message string
-}
-
 // NSCAServer can be used as a lower-level alternative to RunEndpoint. It is NOT safe
 // to use an instance across mutiple threads.
 type NSCAServer struct {
 	Url          string
+	Identity     string
+	Key          string
 	conn         net.Conn
 	serializer   serializers.Serializer
-	serverInfo   ServerInfo
 	CPUWarning   float64
 	CPUCritical  float64
 	DiskWarning  float64
@@ -56,12 +28,10 @@ type NSCAServer struct {
 }
 
 var sampleConfig = `
+  # These all values are server specific.
   Url = "icinga.dev.thermeon.com:5668"
-  ## Data format to output.
-  ## Each data format has its own unique set of configuration options, read
-  ## more about them here:
-  ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_OUTPUT.md
-  ##data_format = "influx"
+  Identity = "system-checker"
+  Key = "0123456789"
   CPUWarning = 3.6
   CPUCritical = 4.0
   DiskWarning = 80.0
@@ -81,44 +51,40 @@ func (n *NSCAServer) Description() string {
 }
 
 // define GetKey and GetIdentity methods
-func getIdentity() string {
-	return "system-checker"
+func (n *NSCAServer) getIdentity() string {
+	return n.Identity
 }
 
-func getKey(id string) ([]byte, error) {
-	return []byte("0123456789"), nil
-}
-
-// create the appropriate TLS configuration
-// note that we specifiy a single cipher suite of type TLS_PSK_*
-// also note that the server requires a certificate, even if not used here
-
-var (
-	config = &tls.Config{
-		CipherSuites: []uint16{psk.TLS_PSK_WITH_AES_256_CBC_SHA},
-		Certificates: []tls.Certificate{tls.Certificate{}},
-		Extra: psk.PSKConfig{
-			GetKey:      getKey,
-			GetIdentity: getIdentity,
-		},
-	}
-)
-
-func generateSessionId() string {
-	randbytes := make([]byte, 6)
-	if _, err := io.ReadFull(rand.Reader, randbytes); err != nil {
-		return ""
-	}
-	return base64.StdEncoding.EncodeToString(randbytes)
+func (n *NSCAServer) getKey(id string) ([]byte, error) {
+	return []byte(n.Key), nil
 }
 
 // Connect to an NSCA server.
 func (n *NSCAServer) Connect() error {
+	config := &tls.Config{
+		CipherSuites: []uint16{psk.TLS_PSK_WITH_AES_256_CBC_SHA},
+		Certificates: []tls.Certificate{tls.Certificate{}},
+		Extra: psk.PSKConfig{
+			GetKey:      n.getKey,
+			GetIdentity: n.getIdentity,
+		},
+	}
 	conn, err := tls.Dial("tcp", n.Url, config)
 	if err != nil {
 		return err
 	}
 	n.conn = conn
+	uuid := uuid.New()
+
+	//commands which nsca-ng server accepts
+	command := "MOIN 1 " + uuid.String()
+	fmt.Fprintf(n.conn, command+"\n")
+
+	s, err := serializers.NewNscaSerializer(n.CPUWarning, n.CPUCritical, n.DiskWarning, n.DiskCritical)
+	if err != nil {
+		return err
+	}
+	n.serializer = s
 	return nil
 }
 
@@ -132,22 +98,13 @@ func (n *NSCAServer) Close() error {
 }
 
 func (n *NSCAServer) Write(metrics []telegraf.Metric) error {
-
-	sessionId := generateSessionId()
-
-	//commands which nsca-ng server accepts
-	command := "MOIN 1 " + sessionId
-	fmt.Fprintf(n.conn, command+"\n")
-
 	var batch []byte
-	s, err := serializers.NewNscaSerializer(n.CPUWarning, n.CPUCritical, n.DiskWarning, n.DiskCritical)
-	if err != nil {
-		return err
-	}
+	var err error
 	for _, metric := range metrics {
-		buf, err := s.Serialize(metric)
+		buf, err := n.serializer.Serialize(metric)
 		if err != nil {
 			fmt.Printf("Error serializing some metrics to nsca: %s", err.Error())
+			return err
 		}
 		batch = append(batch, buf...)
 	}
